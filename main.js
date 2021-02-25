3646185fa96363ed8999123bec5a7d3c874e29ff
Geordi <gjad.bonnie@gmail.com>
2021-02-25 14:18:39 +0100

    Alert als je gewonnen of verloren hebt

--- main.js
+++ main.js
@@ -1,19 +1,63 @@
-let rockJavascript = document.getElementById("rock")
-let paperJavascript = document.getElementById("paper")
-let scissorsJavascript = document.getElementById("scissors")
+let rockJavascript = document.getElementById("rock");
+let paperJavascript = document.getElementById("paper");
+let scissorsJavascript = document.getElementById("scissors");
 
-rockJavascript.addEventListener("click", rockFunction)
-paperJavascript.addEventListener("click", paperFunction)
-scissorsJavascript.addEventListener("click", scissorsFunction)
+    const choices = document.querySelectorAll(".buttons");    
+    const computerChoices = ["Rock", "Paper", "Scissors"];
+    
+function rock() {  
 
-function rockFunction() {
+    const computerNumber = Math.floor(Math.random() * 3);
+    const computerChoice = computerChoices[computerNumber];
+
     document.getElementById("rpsText").innerHTML = "Rock";
-}
+    document.getElementById("compText").innerHTML = computerChoice;
 
-function paperFunction() {
+    if(computerChoice == "Scissors") {
+        alert("You won!");
+    }
+    else if(computerChoice == "Paper") {
+        alert("You lost...")
+    }
+    else if(computerChoice == "Rock") {
+        alert("Same as computer!")
+    }
+};
+    
+function paper() {   
+
+    const computerNumber = Math.floor(Math.random() * 3);
+    const computerChoice = computerChoices[computerNumber];
+
     document.getElementById("rpsText").innerHTML = "Paper";
-}
+    document.getElementById("compText").innerHTML = computerChoice;
 
-function scissorsFunction() {
+    if(computerChoice == "Rock") {
+        alert("You won!");
+    }
+    else if(computerChoice == "Scissors") {
+        alert("You lost...")
+    }
+    else if(computerChoice == "Paper") {
+        alert("Same as computer!")
+    }
+}    
+
+function scissors() {
+
+    const computerNumber = Math.floor(Math.random() * 3);
+    const computerChoice = computerChoices[computerNumber];
+
     document.getElementById("rpsText").innerHTML = "Scissors";
+    document.getElementById("compText").innerHTML = computerChoice;
+
+    if(computerChoice == "Paper") {
+        alert("You won!");
+    }
+    else if(computerChoice == "Rock") {
+        alert("You lost...")
+    }
+    else if(computerChoice == "Scissors") {
+        alert("Same as computer!")
+    }
 }
